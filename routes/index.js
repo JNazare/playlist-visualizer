b2d5a1b218eecfe16931337a80b0f2501ec9fa55
Jessica Bethune <jessica.bethune@gmail.com>
2013-03-03 22:25:29 -0500

    Playlist now split into halves: each half of song results based on different parameters

--- routes/index.js
+++ routes/index.js
@@ -15,24 +15,65 @@ exports.index = function(req, res){
 };
 
 exports.new = function(req, res){
+	var dur = req.body.duration*60;
+	var limit = req.body.duration*60;
 	var T = req.body.tempo.split(" - ");
 	var D = req.body.dance.split(" - ");
 	var H = req.body.hot.split(" - ");
 	var F = req.body.fam.split(" - ");
 	echo('song/search').get({
-	  style: req.body.genre,
-	  mood: req.body.mood,
-	  min_tempo: T[0],
-	  max_tempo:T[1],
-	  song_min_hotttnesss: H[0],
-	  song_max_hotttnesss: H[1],
-	  min_danceability: D[0],
-	  max_danceability: D[1],
-	  artist_min_familiarity: F[0],
-	  artist_max_familiarity: F[1]
+		style: req.body.genre,
+		mood: req.body.mood,
+		min_tempo: Number(T[0])-20,
+		max_tempo: Number(T[0])+20,
+		song_min_hotttnesss: Number(H[0])-0.2,
+		song_max_hotttnesss: Number(H[0])+0.2,
+		min_danceability: Number(D[0])-0.2,
+		max_danceability: Number(D[0])+0.2,
+		artist_min_familiarity: Number(F[0])-0.2,
+		artist_max_familiarity: Number(F[0])+0.2,
+		bucket: ['tracks', 'id:rdio-US','audio_summary'],
 	}, function (err, json) {
-		console.log(json.response.songs[0].id);
-	 	console.log(json.response);
+		var i = 0;
+		var plist = [];
+		while (dur>limit/2) {
+			if(json.response.songs[i].tracks != [] && json.response.songs[i].tracks[0]){
+				dur -= json.response.songs[i].audio_summary.duration;
+				var j = (json.response.songs[i].tracks[0].foreign_id).split(":");
+				var rdio_id = j[2]
+				plist.push(rdio_id);
+			}
+			// plist.push('id: ' + json.response.songs[i].id + ' duration: ' + json.response.songs[i].audio_summary.duration + ' tempo: ' + json.response.songs[i].audio_summary.tempo);
+			i++;
+		}
+		echo('song/search').get({
+			style: req.body.genre,
+			mood: req.body.mood,
+			min_tempo: Number(T[1])-20,
+			max_tempo: Number(T[1])+20,
+			song_min_hotttnesss: Number(H[1])-0.2,
+			song_max_hotttnesss: Number(H[1])+0.2,
+			min_danceability: Number(D[1])-0.2,
+			max_danceability: Number(D[1])+0.2,
+			artist_min_familiarity: Number(F[1])-0.2,
+			artist_max_familiarity: Number(F[1])+0.2,
+			bucket: ['tracks', 'id:rdio-US','audio_summary'],
+		}, function (err, json) {
+			var i = 0;
+			while (dur>60) {
+				if(json.response.songs[i].tracks != [] && json.response.songs[i].tracks[0]){
+					dur -= json.response.songs[i].audio_summary.duration;
+					var j = (json.response.songs[i].tracks[0].foreign_id).split(":");
+					var rdio_id = j[2]
+					plist.push(rdio_id);
+				}
+				// plist.push('id: ' + json.response.songs[i].id + ' duration: ' + json.response.songs[i].audio_summary.duration + ' tempo: ' + json.response.songs[i].audio_summary.tempo);
+				i++;
+			}
+			// console.log(plist);
+			// Replace res.send below with res.render for the rdio page
+			res.send('See terminal for ids');
+		});
 	});
-	res.send('Request received: duration = ' + req.body.duration + 'mins, genres = ' + req.body.genre + ', moods = ' + req.body.mood + ', tempo = ' + req.body.tempo + ', danceability = ' + req.body.dance + ', familiarity = ' + req.body.fam + ', hotness = ' + req.body.hot);
+	// res.send('Request received: duration = ' + req.body.duration + 'mins, genres = ' + req.body.genre + ', moods = ' + req.body.mood + ', tempo = ' + req.body.tempo + ', danceability = ' + req.body.dance + ', familiarity = ' + req.body.fam + ', hotness = ' + req.body.hot);
 }
