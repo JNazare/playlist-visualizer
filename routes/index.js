10833ea143a4484587cfdf819c26d243a59d75da
Jessica Bethune <jessica.bethune@gmail.com>
2013-03-04 17:19:20 -0500

    homepage facelift

--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,15 @@ var echo = echojs({
 });
 
 exports.index = function(req, res){
-	// echo('song/search').get({
-	//   artist: 'zedd',
-	//   title: 'spectrum'
-	// }, function (err, json) {
-	//   console.log(json.response);
-	// });
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Playlist Generator' });
 };
 
 exports.new = function(req, res){
 	var dur = req.body.duration*60;
 	var limit = req.body.duration*60;
-	var T = req.body.tempo.split(" - ");
-	var D = req.body.dance.split(" - ");
-	var H = req.body.hot.split(" - ");
+	var T = [req.body.tempo1, req.body.tempo2];
+	var D = [req.body.dance1, req.body.dance2];
+	var H = [req.body.hot1, req.body.hot2];
 	echo('song/search').get({
 		style: req.body.genre,
 		mood: req.body.mood,
@@ -29,6 +23,7 @@ exports.new = function(req, res){
 		song_max_hotttnesss: Number(H[0])+0.2,
 		min_danceability: Number(D[0])-0.2,
 		max_danceability: Number(D[0])+0.2,
+		results: 100,
 		bucket: ['tracks', 'id:rdio-US','audio_summary'],
 	}, function (err, json) {
 		console.log('response 1', json.response);
@@ -36,13 +31,11 @@ exports.new = function(req, res){
 		var plist = [];
 		while (dur>limit/2) {
 			if(json.response.songs[i].tracks != [] && json.response.songs[i].tracks[0]){
-				console.log('i= ', i);
 				dur -= json.response.songs[i].audio_summary.duration;
 				var j = (json.response.songs[i].tracks[0].foreign_id).split(":");
 				var rdio_id = j[2]
 				plist.push(rdio_id);
 			}
-			// plist.push('id: ' + json.response.songs[i].id + ' duration: ' + json.response.songs[i].audio_summary.duration + ' tempo: ' + json.response.songs[i].audio_summary.tempo);
 			i++;
 		}
 		console.log('starting search 2');
@@ -55,6 +48,7 @@ exports.new = function(req, res){
 			song_max_hotttnesss: Number(H[1])+0.2,
 			min_danceability: Number(D[1])-0.2,
 			max_danceability: Number(D[1])+0.2,
+			results: 100,
 			bucket: ['tracks', 'id:rdio-US','audio_summary'],
 		}, function (err, json) {
 			console.log('response 2', json.response);
@@ -66,13 +60,11 @@ exports.new = function(req, res){
 					var rdio_id = j[2]
 					plist.push(rdio_id);
 				}
-				// plist.push('id: ' + json.response.songs[i].id + ' duration: ' + json.response.songs[i].audio_summary.duration + ' tempo: ' + json.response.songs[i].audio_summary.tempo);
 				i++;
 			}
-			// console.log(plist);
+			console.log(plist);
 			// Replace res.send below with res.render for the rdio page
 			res.send('See terminal for ids');
 		});
 	});
-	// res.send('Request received: duration = ' + req.body.duration + 'mins, genres = ' + req.body.genre + ', moods = ' + req.body.mood + ', tempo = ' + req.body.tempo + ', danceability = ' + req.body.dance + ', familiarity = ' + req.body.fam + ', hotness = ' + req.body.hot);
 }
